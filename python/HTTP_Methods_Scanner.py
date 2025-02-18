46021336fb6a32d2c7cf9ebe723bf17b59db6dc7
SimeoneCristofaro <42369172+SimeoneCristofaro@users.noreply.github.com>
2025-02-18 09:22:14 -0500

    Add files via upload

---
+++ python/HTTP_Methods_Scanner.py
@@ -0,0 +1,34 @@
+import http.client
+
+http_methods = ('GET', 'POST', 'DELETE', 'PATCH', 'OPTIONS', 'PUT', 'HEAD', 'CONNECT', 'TRACE')
+
+host = input('Inserire URL completa del sistema target (default:192.168.100.110/phpMyAdmin/index.php):')
+
+if host == "":
+    host = "192.168.100.110/phpMyAdmin/index.php"
+
+url_path = ""
+url_domain = host.split('/')[0]
+
+for i in range(1, len(host.split('/'))):
+    url_path = url_path + "/" + host.split('/')[i]
+
+port = input('Inserire la porta del sistema target (default:80): ')
+
+if(port == ""):
+    port = 80
+
+for i in range(len(http_methods)):
+    try:
+        connection = http.client.HTTPConnection(url_domain,port)
+        connection.request(http_methods[i], url_path)
+        response = connection.getresponse()
+        print(http_methods[i] + " : ", response.status)
+        if(response.status == 200):
+            print("metodo ammesso\n")
+        else:
+            print("metodo non ammesso\n")
+        connection.close()
+    except ConnectionRefusedError:
+        print("Connessione fallita")
+
